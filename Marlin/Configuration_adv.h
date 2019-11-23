0988913eda2a1f115711d14db5b83ed4785ae280
alan <github@squiddity.com>
2019-11-23 13:34:48 -0800

    activating 512k build with game fodder

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1184,9 +1184,9 @@
   //#define BOOT_MARLIN_LOGO_ANIMATED // Animated Marlin logo. Costs ~‭3260 (or ~940) bytes of PROGMEM.
 
   // Frivolous Game Options
-  //#define MARLIN_BRICKOUT
-  //#define MARLIN_INVADERS
-  //#define MARLIN_SNAKE
+  #define MARLIN_BRICKOUT
+  #define MARLIN_INVADERS
+  #define MARLIN_SNAKE
   //#define GAMES_EASTER_EGG          // Add extra blank lines above the "Games" sub-menu
 
 #endif // HAS_GRAPHICAL_LCD

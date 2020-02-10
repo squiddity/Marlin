a3a9a2c5ec77e2a137b06071123b13d8f86b40a2
alan <github@squiddity.com>
2020-02-09 21:09:30 -0800

    turn on linear advance, commit current Z offset to code

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -937,7 +937,7 @@
  * Specify a Probe position as { X, Y, Z }
  */
 //#define NOZZLE_TO_PROBE_OFFSET { 10, 10, 0 }
-#define NOZZLE_TO_PROBE_OFFSET { -44, -16, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -44, -16, -2.27 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
